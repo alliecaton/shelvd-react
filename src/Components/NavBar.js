cbe7a57783ee7bd36822db9dac6a1a4556e63d05
Allie <alliecaton3@gmail.com>
2021-06-30 17:00:21 -0400

    logo and last daily commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import image from "../images/book.png";
 
 const NavBar = () => {
   return (
     <div>
       <ul>
+        <li>
+          <img src={image} alt="book logo" />
+        </li>
         <li className="navbar">link</li>
         <li className="navbar">link</li>
       </ul>
