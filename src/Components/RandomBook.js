820458f02c1e1570f27b1a50243932de9dec617e
Allie <alliecaton3@gmail.com>
2021-07-01 19:31:48 -0400

    setting up search bar

--- src/Components/RandomBook.js
+++ src/Components/RandomBook.js
@@ -8,31 +8,31 @@ const RandomBook = () => {
 		},
 	});
 
-	const fetchBook = () => {
-		fetch(
-			'https://www.googleapis.com/books/v1/volumes?q=fiction+subject&printType=books&orderBy=relevance'
-		)
-			.then(resp => resp.json())
-			.then(json => {
-				let num = Math.floor(Math.random() * (json.items.length - 1 + 1) + 1);
-				let index = Math.floor(
-					Math.random() * (json.totalItems / 10 - 1 + 1) + 1
-				);
-				fetch(
-					`https://www.googleapis.com/books/v1/volumes?q=fiction+subject&startIndex=${index}&maxResults=10`
-				)
-					.then(resp => resp.json())
-					.then(json => {
-						setState(json.items[num]);
-					});
-			});
-	};
+	// const fetchBook = () => {
+	// 	fetch(
+	// 		'https://www.googleapis.com/books/v1/volumes?q=fiction+subject&printType=books&orderBy=relevance'
+	// 	)
+	// 		.then(resp => resp.json())
+	// 		.then(json => {
+	// 			let num = Math.floor(Math.random() * (json.items.length - 1 + 1) + 1);
+	// 			let index = Math.floor(
+	// 				Math.random() * (json.totalItems / 10 - 1 + 1) + 1
+	// 			);
+	// 			fetch(
+	// 				`https://www.googleapis.com/books/v1/volumes?q=fiction+subject&startIndex=${index}&maxResults=10`
+	// 			)
+	// 				.then(resp => resp.json())
+	// 				.then(json => {
+	// 					setState(json.items[num]);
+	// 				});
+	// 		});
+	// };
 
 	console.log('state', bookData);
 	return (
 		<>
 			<div className='book-card'>{bookData.volumeInfo.title}</div>
-			<Button text={'Get Random Book!!'} onClick={fetchBook} />
+			<Button text={'Get Random Book!!'} />
 		</>
 	);
 };
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Searchbar = () => {
+	const [query, setState] = useState('');
+
+	const handleInput = e => {
+		setState(e.target.value);
+		console.log(query);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		fetch(
+			'https://www.googleapis.com/books/v1/volumes?q=${query}+inTitle&printType=books&orderBy=relevance'
+		)
+			.then(resp => resp.json())
+			.then(json => {
+				console.log(json);
+			});
+	};
+
 	return (
-		<div>
+		<>
 			<form className='searchbar'>
-				<input type='text' />
-				<input type='submit' value='Submit' />
+				<input type='text' onChange={e => handleInput(e)} value={query} />
+				<input type='submit' value='Submit' onClick={e => handleSubmit(e)} />
 			</form>
-		</div>
+		</>
 	);
 };
 
