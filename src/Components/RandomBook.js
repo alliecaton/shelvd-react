7141bc53a4c14400bcae9079b6345754090da331
Allie <alliecaton3@gmail.com>
2021-07-01 11:20:27 -0400

    working on random book component state

--- src/Components/RandomBook.js
+++ src/Components/RandomBook.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import Button from "./Button";
 
 const RandomBook = () => {
-  const [bookData] = useState([]);
+  const [bookData, setState] = useState([]);
 
   const fetchBook = () => {
     fetch("https://www.googleapis.com/books/v1/volumes?q=fiction+subject")
       .then((resp) => resp.json())
-      .then((json) => console.log(json));
+      .then((json) => setState(json));
   };
-
+  console.log("this is the state", bookData.items);
   return (
     <>
       <div className="book-card">{bookData}</div>
