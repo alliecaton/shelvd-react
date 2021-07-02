d95d92f298de476ff4bf1c95bea67487535f7f4d
Allie <alliecaton3@gmail.com>
2021-07-02 17:35:41 -0400

    final styling of the day

--- src/App.js
+++ src/App.js
@@ -7,18 +7,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import BookListContainer from './containers/BookListContainer'
 
 function App() {
-	const storedDarkMode = localStorage.getItem('DARK_MODE')
-	console.log('local', storedDarkMode)
+	const [dark, setState] = useState(false)
+	const changeMode = () => setState(dark ? false : false)
 
-	const [dark, setState] = useState(storedDarkMode)
-	const changeMode = () => setState(dark ? false : true)
-
-	useEffect(() => {
-		console.log('dark mode?', dark)
-		localStorage.setItem('DARK_MODE', dark)
-	}, [dark])
-
-	console.log('render', dark)
 	return (
 		<div className='App' data-theme={dark ? 'dark' : 'light'}>
 			<Router>
