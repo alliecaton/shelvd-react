eec1ef49549a7b365615648f9dc67d0591a40716
Allie <alliecaton3@gmail.com>
2021-07-02 11:27:58 -0400

    implementing search bar. going to start implementing redux now

--- src/Containers/Searchbar.js
+++ src/Containers/Searchbar.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import BookListContainer from './BookListContainer';
+import { Link } from 'react-router-dom';
 
 const Searchbar = () => {
 	const [state, setState] = useState({
@@ -25,14 +27,30 @@ const Searchbar = () => {
 					...prevState,
 					queryResults: json.items,
 				}));
+				renderBooks();
 			});
 	};
-	console.log(state);
+
+	const renderBooks = () => {
+		console.log('hit');
+		return (
+			<div>
+				<Link to='/books'>
+					<BookListContainer
+						queryResults={state.queryReults}
+						query={state.query}
+					/>
+				</Link>
+			</div>
+		);
+	};
+
 	return (
 		<>
 			<form className='searchbar'>
 				<input type='text' onChange={e => handleInput(e)} value={state.query} />
 				<input type='submit' value='Submit' onClick={e => handleSubmit(e)} />
+				{renderBooks}
 			</form>
 		</>
 	);
