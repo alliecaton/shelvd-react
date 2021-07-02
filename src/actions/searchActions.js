8cda6b7a7659d4c25c45e6a5cd569503a051cb8f
Allie <alliecaton3@gmail.com>
2021-07-02 14:58:30 -0400

    working on fetch'

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,8 +1,9 @@
 export const fetchResults = query => {
 	return dispatch => {
-		fetch('http://localhost:3001/results')
+		fetch(`http://localhost:3001/results/${query}`)
 			.then(r => r.json())
 			.then(json => {
+				console.log('returned json', json.results)
 				const data = { query: query, json: json.results }
 				console.log(json)
 				dispatch({ type: 'GET_RESULTS', payload: data })
